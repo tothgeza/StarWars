35d0be39b32cfe4297f1be44b0c8bf3d71de7b90
tothgeza <toth.geza.0425@gmail.com>
2021-07-27 06:03:02 +0200

    Implement Nav function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import "../src/style.scss";
+import People from "./People";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="border d-sm-flex flex-column h-100">
+        <div class="twinkling"></div>
+        <Nav />
+        <Switch>
+          <Route path="/people" component={People} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
