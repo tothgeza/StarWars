bf6617183ef02b390d34faf8008974762399018f
tothgeza <toth.geza.0425@gmail.com>
2021-08-13 23:35:37 +0200

    Fix container margin

--- src/components/PostsFilms.js
+++ src/components/PostsFilms.js
@@ -14,7 +14,7 @@ export default function PostsPeople({ posts, loading }) {
 	}
 
 	return (
-		<div className="container-xxl">
+		<div className="container-xxl mb-5">
 			<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xxl-5 justify-content-center">
 				{posts.results.map((post) => (
 					<div className="col-auto mb-2 d-flex justify-content-center">
