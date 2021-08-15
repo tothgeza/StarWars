8513a0f98ab94b3c78fffb3eebac20bade8667b8
tothgeza <toth.geza.0425@gmail.com>
2021-08-15 20:44:49 +0200

    Refactor images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,7 @@ import React from "react";
 import Loader from "../utils/Loader";
 import Card from "./Card";
 
-export default function Cards({ posts, loading, type , rowNumber}) {
+export default function Cards({ posts, loading, typeOfCategory, rowNumber }) {
 	if (loading) {
 		return <Loader />;
 	}
@@ -11,7 +11,11 @@ export default function Cards({ posts, loading, type , rowNumber}) {
 		<div className="container-xxl mb-5">
 			<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 justify-content-center">
 				{posts.results.map((post) => (
-					<Card post={post} type={type} rowNumber={rowNumber} />
+					<Card
+						post={post}
+						typeOfCategory={typeOfCategory}
+						rowNumber={rowNumber}
+					/>
 				))}
 			</div>
 		</div>
